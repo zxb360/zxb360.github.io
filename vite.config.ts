754a96e3db64fa6abddd7068667ddc549c5dfb58
jaeder <zxb360@gmail.com>
2025-07-22 12:28:18 -0300

    alteracao no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/',
+  plugins: [react(),
+    {
+      name: 'adjust-paths',
+      enforce: 'post',
+      apply: 'build',
+      transformIndexHtml(html) {
+        return html.replace(/src="\//g, 'src="')
+                   .replace(/href="\//g, 'href="')
+      }
+    }
+  ],
 })
